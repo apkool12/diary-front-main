c059a068f53e6e85531d35e93c062c9acc92cfbe
우은식 <apkool12@naver.com>
2024-09-01 16:57:01 +0900

    nav dropdown add

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,30 @@
-import React from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import './Navbar.css'; 
+import './Navbar.css';
 
 function Navbar() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false); // 로그인 상태
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false); // 드롭다운 상태
+  const dropdownRef = useRef(null); // 드롭다운 참조
+
+  const toggleDropdown = () => {
+    setIsDropdownOpen(prevState => !prevState);
+  };
+
+  // 드롭다운 외부 클릭 시 드롭다운 닫기
+  const handleClickOutside = (e) => {
+    if (dropdownRef.current && !dropdownRef.current.contains(e.target)) {
+      setIsDropdownOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, []);
+
   return (
     <header className="navbar">
       <nav>
@@ -12,8 +34,30 @@ function Navbar() {
           <li><Link className="nav-link" to="/devlog">Dev Log</Link></li>
         </ul>
         <div className="auth-buttons">
-          <div><Link className="nav-link button" to="/login">로그인</Link></div>
-          <div><Link className="nav-link button" to="/signup">회원가입</Link></div>
+          <div
+            className="user-button"
+            onClick={toggleDropdown}
+            ref={dropdownRef}
+          >
+            <span>유저</span>
+            {isDropdownOpen && (
+              <div className="dropdown-menu">
+                {isLoggedIn ? (
+                  <>
+                    <Link className="nav-link" to="/logout">로그아웃</Link>
+                    <Link className="nav-link" to="/profile">회원정보</Link>
+                    <Link className="nav-link" to="/settings">설정</Link>
+                    <Link className="nav-link" to="/projects">프로젝트</Link>
+                  </>
+                ) : (
+                  <>
+                    <Link className="nav-link" to="/login">로그인</Link>
+                    <Link className="nav-link" to="/signup">회원가입</Link>
+                  </>
+                )}
+              </div>
+            )}
+          </div>
         </div>
       </nav>
     </header>
